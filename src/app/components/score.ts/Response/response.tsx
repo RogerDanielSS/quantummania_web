35455971e25f497d344abf9c3fcd9f3f07b80e57
rogerdanielss <danielsimoesp@outlook.com>
2025-06-08 18:28:09 -0300

    fix: remove log

--- src/app/components/score.ts/Response/response.tsx
+++ src/app/components/score.ts/Response/response.tsx
@@ -41,7 +41,6 @@ export default function Response({
           level={level}
           showRightResponse
           preSettedGivenResponse={givenResponse.reponse}
-          onGoFurther={(): void => console.log("further")}
         />
       )}
     </>
